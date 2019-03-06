7bda8a174abd036e2e043d609ecf26c064953eb3
Адылканов Муратжан Талгатович <@corp.itstep>
2019-03-06 18:50:01 +0600

    Сделали домашнюю работу

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public string Mark { get; set; }
+        public double EngineCapacity { get; set; }
+        public string Body { get; set; }
+        public string DriveUnit { get; set; }
+        public int ReleaseDate { get; set; }
+    }
+}
